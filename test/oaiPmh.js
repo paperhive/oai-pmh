29406b433f444d74d855d0da3fc7be11f28f6ab9
André Gaul <andre@gaul.io>
2016-02-06 20:21:42 +0100

    group test

--- test/oaiPmh.js
+++ test/oaiPmh.js
@@ -4,17 +4,19 @@ import { mochaAsync } from './utils';
 const baseUrl = 'http://export.arxiv.org/oai2';
 
 describe('OaiPmh', () => {
-  it('should identify arxiv', mochaAsync(function* () {
-    const oaiPmh = new OaiPmh(baseUrl);
-    const res = yield oaiPmh.identify();
-    res.should.containDeep({
-      repositoryName: 'arXiv',
-      baseURL: 'http://export.arxiv.org/oai2',
-      protocolVersion: '2.0',
-      adminEmail: 'help@arxiv.org',
-      earliestDatestamp: '2007-05-23',
-      deletedRecord: 'persistent',
-      granularity: 'YYYY-MM-DD',
-    });
-  }));
+  describe('identify()', () => {
+    it('should identify arxiv', mochaAsync(function* () {
+      const oaiPmh = new OaiPmh(baseUrl);
+      const res = yield oaiPmh.identify();
+      res.should.containDeep({
+        repositoryName: 'arXiv',
+        baseURL: 'http://export.arxiv.org/oai2',
+        protocolVersion: '2.0',
+        adminEmail: 'help@arxiv.org',
+        earliestDatestamp: '2007-05-23',
+        deletedRecord: 'persistent',
+        granularity: 'YYYY-MM-DD',
+      });
+    }));
+  });
 });

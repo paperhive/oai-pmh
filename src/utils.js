ab7092215cf0adf1a38c468d12e949aaf5f6b366
André Gaul <andre@gaul.io>
2018-06-21 16:04:13 +0200

    move xml function to own file

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-import { promisify } from 'bluebird'
-import { get } from 'lodash'
-import { parseString } from 'xml2js'
-
 // error class for OAI-PMH errors
 export class OaiPmhError extends Error {
   constructor (message, code) {
@@ -11,32 +7,6 @@ export class OaiPmhError extends Error {
     this.code = code
     Error.captureStackTrace(this, this.constructor.name)
   }
-}
-
-// test if the parsed xml contains an error
-export async function parseOaiPmhXml (xml) {
-  // parse xml into js object
-  const obj = await promisify(parseString)(xml, {
-    explicitArray: false,
-    trim: true,
-    normalize: true
-  })
-
-  const oaiPmh = obj && obj['OAI-PMH']
-
-  if (!oaiPmh) {
-    throw new OaiPmhError('Returned data does not conform to OAI-PMH')
-  }
-
-  const error = oaiPmh.error
-  if (error) {
-    throw new OaiPmhError(
-      `OAI-PMH provider returned an error: ${error._}`,
-      get(error, '$.code')
-    )
-  }
-
-  return oaiPmh
 }
 
 export async function sleep (seconds) {
